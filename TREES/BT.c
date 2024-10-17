6b38b59f2e7a7fbfbb0c06fb9f038eff19a49e94
2305744 <2305744@kiit.ac.in>
2024-10-17 12:50:26 +0530

    initial commit

--- TREES/BT.c
+++ TREES/BT.c
@@ -30,52 +30,6 @@ struct Node* buildTree(struct Node* root){
     root->right = buildTree(root->right);
     return root;
 }
-struct Queue {
-    int front, rear, size;
-    unsigned capacity;
-    struct Node** array;
-};
-struct Queue* createQueue(unsigned capacity) {
-    struct Queue* queue = (struct Queue*)malloc(sizeof(struct Queue));
-    queue->capacity = capacity;
-    queue->front = queue->size = 0;
-    queue->rear = capacity - 1;
-    queue->array = (struct Node**)malloc(queue->capacity * sizeof(struct Node*));
-    return queue;
-}
-int isFull(struct Queue* queue) { return (queue->size == queue->capacity); }
-int isEmpty(struct Queue* queue) { return (queue->size == 0); }
-void enqueue(struct Queue* queue, struct Node* item) {
-    if (isFull(queue)) return;
-    queue->rear = (queue->rear + 1) % queue->capacity;
-    queue->array[queue->rear] = item;
-    queue->size = queue->size + 1;
-}
-struct Node* dequeue(struct Queue* queue) {
-    if (isEmpty(queue)) return NULL;
-    struct Node* item = queue->array[queue->front];
-    queue->front = (queue->front + 1) % queue->capacity;
-    queue->size = queue->size - 1;
-    return item;
-}
-void levelOrderTraversal(struct Node* root) {
-    if (root == NULL) return;
-    struct Queue* queue = createQueue(1000);  
-    enqueue(queue, root);
-    while (!isEmpty(queue)) {
-        int nodeCount = queue->size; 
-        while (nodeCount > 0) {
-            struct Node* node = dequeue(queue);
-            printf("%d ", node->data);
-            if (node->left != NULL)
-                enqueue(queue, node->left);
-            if (node->right != NULL)
-                enqueue(queue, node->right);
-            nodeCount--;
-        }
-        printf("\n");
-    }
-}
 void inorderTraversal(struct Node* root) {
     if (root == NULL) {
         return;
@@ -103,8 +57,6 @@ void postorderTraversal(struct Node* root) {
 int main() {
     struct Node* root = NULL;
     root = buildTree(root);
-    printf("Level Order Traversal:\n");
-    levelOrderTraversal(root);
     printf("Inorder Traversal of the binary tree is: ");
     inorderTraversal(root); 
     printf("\n");
